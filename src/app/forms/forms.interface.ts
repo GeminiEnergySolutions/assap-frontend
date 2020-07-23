abddecbd5b2421a5b710e24c80ead6f9168740ae
Adrian Kunz <clashsoft@hotmail.com>
2020-07-23 22:43:28 +0200

    Add FormsService.loadSchema

--- src/app/forms/forms.interface.ts
+++ src/app/forms/forms.interface.ts
@@ -1,4 +1,4 @@
-export interface Form {
+export interface Schema {
   geminiForm: Section[];
 }
 
--- src/app/forms/forms.service.ts
+++ src/app/forms/forms.service.ts
@@ -1,10 +1,19 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Schema} from "./forms.interface";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormsService {
 
-  constructor() {
+  constructor(
+    private http: HttpClient,
+  ) {
+  }
+
+  loadSchema(name: string): Observable<Schema> {
+    return this.http.get<Schema>(`/assets/${name}.json`);
   }
 }
