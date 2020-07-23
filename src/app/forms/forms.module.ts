5cdd92e4409e3eef2096a2e4f494159c52d9d2fb
Adrian Kunz <clashsoft@hotmail.com>
2020-07-23 22:43:55 +0200

    Add form component

--- src/app/forms/forms.module.ts
+++ src/app/forms/forms.module.ts
@@ -1,11 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MasterDetailComponent} from './master-detail/master-detail.component';
+import { FormComponent } from './form/form.component';
 
 
 @NgModule({
   declarations: [
     MasterDetailComponent,
+    FormComponent,
   ],
   exports: [
     MasterDetailComponent,
