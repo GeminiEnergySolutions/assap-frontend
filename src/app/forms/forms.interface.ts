aedfeea9a53fdfc8560cbdcf12c472cebff82930
Adrian Kunz <clashsoft@hotmail.com>
2020-07-23 22:37:49 +0200

    Add forms.interface.ts

---
+++ src/app/forms/forms.interface.ts
@@ -0,0 +1,23 @@
+export interface Form {
+  geminiForm: Section[];
+}
+
+export interface Section {
+  index: number;
+  section: string;
+  elements: Element[];
+  id: string;
+}
+
+export type DataType = 'pickerinputrow' | 'emailrow' | 'phonerow' | 'textrow' | 'textarearow' | 'introw' | 'decimalrow';
+export type ValidationType = 'mandatory' | 'optional';
+
+export interface Element {
+  index: number;
+  hint: string;
+  dataType: DataType;
+  defaultValues: string;
+  param: string;
+  validation: ValidationType;
+  id: string;
+}
