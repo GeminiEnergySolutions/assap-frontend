fd5fc6b96aff0df0bb0ff44f1d029b8de61d7e6a
Adrian Kunz <clashsoft@hotmail.com>
2020-08-04 22:01:23 +0200

    Implement type component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import {RouterModule, Routes} from '@angular/router';
 import {MainComponent} from "./main/main.component";
 import {ZoneComponent} from "./zone/zone.component";
 import {PreAuditComponent} from "./pre-audit/pre-audit.component";
+import {TypeComponent} from "./type/type.component";
 
 
 const routes: Routes = [
@@ -10,6 +11,7 @@ const routes: Routes = [
   {path: 'audits', component: PreAuditComponent },
   {path: 'audits/:aid', component: PreAuditComponent },
   {path: 'audits/:aid/zones/:zid', component: ZoneComponent},
+  {path: 'audits/:aid/zones/:zid/types/:type/:tid', component: TypeComponent},
 ];
 
 @NgModule({
--- src/app/type/type.component.ts
+++ src/app/type/type.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Audit, Type, Zone} from "../model/audit.interface";
+import {Subscription} from "rxjs";
+import {ParseService} from "../parse/parse.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-type',
   templateUrl: './type.component.html',
   styleUrls: ['./type.component.scss']
 })
-export class TypeComponent implements OnInit {
+export class TypeComponent implements OnInit, OnDestroy {
+  audit?: Audit;
+  zone?: Zone;
+  typeName?: string;
+  types: Type[] = [];
+  selectedType?: Type;
 
-  constructor() { }
+  subscription: Subscription;
 
-  ngOnInit(): void {
+  constructor(
+    private parseService: ParseService,
+    private route: ActivatedRoute,
+  ) {
   }
 
+  ngOnInit(): void {
+    this.subscription = this.route.params.subscribe(params => {
+      const aid: string = params.aid;
+      const zid: number = +params.zid;
+      this.typeName = params.type;
+      const tid: number = +params.tid;
+      this.parseService.getAudits({auditId: aid}).subscribe(audits => {
+        this.audit = audits[0];
+        this.zone = this.audit.zone[zid];
+        this.types = Object.values(this.audit.type).filter(t => t.type === this.typeName);
+        this.selectedType = this.audit.type[tid];
+      });
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
