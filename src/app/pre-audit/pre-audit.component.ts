aa7eb4236f537a808ebd25b64a4e03b2abdf17b9
Adrian Kunz <clashsoft@hotmail.com>
2020-08-04 13:37:12 +0200

    Audit selection

--- src/app/pre-audit/pre-audit.component.ts
+++ src/app/pre-audit/pre-audit.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Audit} from "../model/audit.interface";
 import {ParseService} from "../parse/parse.service";
+import {Feature} from "../model/feature.interface";
 
 @Component({
   selector: 'app-pre-audit',
@@ -10,6 +11,7 @@ import {ParseService} from "../parse/parse.service";
 export class PreAuditComponent implements OnInit {
   audits: Audit[] = [];
   selectedAudit?: Audit;
+  selectedFeatures: Feature[] = [];
 
   constructor(
     private parseService: ParseService,
@@ -19,6 +21,14 @@ export class PreAuditComponent implements OnInit {
   ngOnInit(): void {
     this.parseService.getAudits().subscribe(audits => {
       this.audits = audits;
+    });
+  }
+
+  selectAudit(audit: Audit) {
+    this.selectedAudit = audit;
+
+    this.parseService.getFeatures({auditId: audit.auditId, zoneId: "null"}).subscribe(features => {
+      this.selectedFeatures = features;
     });
   }
 }
