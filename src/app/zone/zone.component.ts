37579283de9b5cde12a0c9d920a3cff4a1353fb4
Adrian Kunz <clashsoft@hotmail.com>
2020-08-04 20:16:36 +0200

    Implement zone component

--- src/app/zone/zone.component.ts
+++ src/app/zone/zone.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Audit, Type, Zone} from "../model/audit.interface";
+import {ParseService} from "../parse/parse.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-zone',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./zone.component.scss']
 })
 export class ZoneComponent implements OnInit {
+  audit?: Audit;
+  zones: Zone[] = [];
+  types: Type[] = [];
+  selectedZone?: Zone;
 
-  constructor() { }
+  activeTab: string;
 
-  ngOnInit(): void {
+  constructor(
+    private parseService: ParseService,
+    private route: ActivatedRoute,
+  ) {
   }
 
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      const aid: string = params.aid;
+      const zid: number = +params.zid;
+      this.parseService.getAudits({auditId: aid}).subscribe(audits => {
+        this.audit = audits[0];
+        this.zones = Object.values(this.audit.zone);
+        this.selectZone(zid);
+      });
+    });
+  }
+
+  private selectZone(zoneId: number) {
+    this.selectedZone = this.zones.find(a => a.id === zoneId);
+    this.types = this.selectedZone?.typeId.map(tid => this.audit.type[tid]) ?? [];
+  }
 }
