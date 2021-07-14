796c1a57f24e582cdad2a8a93a7fd2116ef22d7a
Adrian Kunz <clashsoft@hotmail.com>
2021-07-14 13:50:26 +0200

    fix: Show form for new Types

--- src/app/type/type.component.ts
+++ src/app/type/type.component.ts
@@ -1,9 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
-import {switchMap} from 'rxjs/operators';
+import {forkJoin} from 'rxjs';
+import {map, switchMap} from 'rxjs/operators';
+import {AuditService} from '../audit.service';
 import {FeatureService} from '../feature.service';
 import {Schema} from '../forms/forms.interface';
 import {FeatureData} from '../model/feature.interface';
+import {Types} from '../model/types';
 
 @Component({
   selector: 'app-type',
@@ -13,8 +16,10 @@ import {FeatureData} from '../model/feature.interface';
 export class TypeComponent implements OnInit {
   data?: FeatureData;
   schema?: Schema;
+  schemaId?: string;
 
   constructor(
+    private auditService: AuditService,
     private featureService: FeatureService,
     private route: ActivatedRoute,
   ) {
@@ -22,12 +27,19 @@ export class TypeComponent implements OnInit {
 
   ngOnInit(): void {
     this.route.params.pipe(
-      switchMap(({aid, zid, tid}) => this.featureService.findAll({
-        auditId: aid,
-        zoneId: zid,
-        typeId: tid,
-      })),
-    ).subscribe(features => {
+      switchMap(({aid, zid, tid}) => forkJoin([
+        this.auditService.findAll({auditId: aid}).pipe(map(audits => audits[0].type[tid])),
+        this.featureService.findAll({
+          auditId: aid,
+          zoneId: zid,
+          typeId: tid,
+        }),
+      ])),
+    ).subscribe(([type, features]) => {
+      const type1 = Types.find(t => t.name === type.type);
+      const type2 = type.subtype ? type1.subTypes.find(t => t.name === type.subtype) : type1;
+      this.schemaId = type2.id;
+
       const feature = features[0];
       this.schema = feature ? this.featureService.feature2Schema(feature) : undefined;
       this.data = feature ? this.featureService.feature2Data(feature) : {};
