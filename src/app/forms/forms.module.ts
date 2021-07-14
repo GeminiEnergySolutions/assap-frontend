1ac1b4c194aeb9aec9636f749afb011602db95d5
Adrian Kunz <clashsoft@hotmail.com>
2021-07-15 00:11:50 +0200

    feat: Use accordion for forms

--- src/app/forms/forms.module.ts
+++ src/app/forms/forms.module.ts
@@ -2,6 +2,7 @@ import {CommonModule} from '@angular/common';
 import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {FormsModule as NgFormsModule} from '@angular/forms';
+import {NgbAccordionModule} from '@ng-bootstrap/ng-bootstrap';
 import {FormInputTypePipe} from './form-input-type.pipe';
 import {FormComponent} from './form/form.component';
 
@@ -17,6 +18,7 @@ import {FormComponent} from './form/form.component';
     CommonModule,
     HttpClientModule,
     NgFormsModule,
+    NgbAccordionModule,
   ],
 })
 export class FormsModule {
