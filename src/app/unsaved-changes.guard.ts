6500102caf86f4ba8647e1feacc3d4cb5e825c71
Adrian Kunz <a.kunz@uni-kassel.de>
2024-02-01 22:01:46 +0100

    refactor: Make UnsavedChangesGuard a CanDeactivateFn

--- src/app/unsaved-changes.guard.ts
+++ src/app/unsaved-changes.guard.ts
@@ -1,14 +1,10 @@
-import {Injectable} from '@angular/core';
-
 import {Observable} from 'rxjs';
+import {CanDeactivateFn} from '@angular/router';
 
 export interface SaveableChangesComponent {
   isSaved(): boolean | Observable<boolean> | Promise<boolean>;
 }
 
-@Injectable({ providedIn: 'root' })
-export class UnsavedChangesGuard  {
-  canDeactivate(component: SaveableChangesComponent): Observable<boolean> | Promise<boolean> | boolean {
-    return component.isSaved() || confirm('Are you sure you want to leave this page? Changes that you made may not be saved.');
-  }
-}
+export const UnsavedChangesGuard: CanDeactivateFn<SaveableChangesComponent> = component => {
+  return component.isSaved() || confirm('Are you sure you want to leave this page? Changes that you made may not be saved.');
+};
