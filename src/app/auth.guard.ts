8aff44e8108196a88114d9ffe1fa22f4b3d75dad
Adrian Kunz <a.kunz@uni-kassel.de>
2024-02-01 21:57:15 +0100

    refactor: Make AuthGuard a CanActivateFn

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,26 +1,17 @@
-import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService } from './shared/services/auth.service';
+import {CanActivateFn, Router} from '@angular/router';
+import {inject} from '@angular/core';
+import {AuthService} from './shared/services/auth.service';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class AuthGuard  {
-  constructor(private authService: AuthService, private router: Router) {}
-
-  canActivate(): boolean {
-    // Replace this with your actual login check logic
-    // For example, you can use a service to check if the user is logged in
-    const token = localStorage.getItem('accessToken');
-
-    if (token) {
-      this.authService.getUser().subscribe((res: any) => {
-        this.authService.currentLoginUser = res;
-      });
-      return true;
-    } else {
-      this.router.navigate(['/auth/login']);
-      return false;
-    }
+export const AuthGuard: CanActivateFn = () => {
+  const token = localStorage.getItem('accessToken');
+  if (token) {
+    const authService = inject(AuthService);
+    authService.getUser().subscribe((res: any) => {
+      authService.currentLoginUser = res;
+    });
+    return true;
+  } else {
+    inject(Router).navigate(['/auth/login']);
+    return false;
   }
-}
+};
