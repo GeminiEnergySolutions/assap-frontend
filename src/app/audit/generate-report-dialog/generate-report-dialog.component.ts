e1103ce63abce578d504c8280beb05ff005624c0
Adrian Kunz <a.kunz@uni-kassel.de>
2024-02-01 19:53:13 +0100

    chore: Remove unused code from generate report dialog

--- src/app/audit/generate-report-dialog/generate-report-dialog.component.ts
+++ src/app/audit/generate-report-dialog/generate-report-dialog.component.ts
@@ -22,28 +22,14 @@ export class GenerateReportDialogComponent {
     private authService: AuthService,
     private route: ActivatedRoute
   ) {
-    // this.getUtilityInfo();
     this.auditService.isCompleted = true;
   }
-
-  // getUtilityInfo() {
-  //   this.auditService
-  //     .getUtilityInfo(this.dialogData.auditId)
-  //     .subscribe((res: any) => {
-  //       if (res && res.id) {
-  //         this.formData = { ...this.formData, ...res };
-  //       }
-  //     });
-  // }
 
   changeNav(type: any) {
     if (this.tabType !== type) {
       this.formData = { type };
       this.tabType = type;
     }
-    // if (type === 'energyAudit') {
-    //   this.getUtilityInfo();
-    // }
   }
 
   onCloseDialog() {
@@ -56,42 +42,6 @@ export class GenerateReportDialogComponent {
 
   reportType = "auditEnergy";
   generateReport(type: string) {
-    // this.reportType = type;
-    // let url = this.serverUrl + type + "/" + this.dialogData.auditId + '/'
     window.location.href = this.serverUrl + type + '?auditId=' + this.dialogData.auditId + '&auth_token=' + this.authService.getAuthToken();
-    // this.auditService.isCompleted = false;
-    // this.route.params.pipe(
-    //     switchMap(({ aid }) => {
-    //       if (type == "auditEnergy")
-    //         return this.auditService.auditEnergyReport(this.dialogData.auditId);
-    //       else if (type == "feasibility")
-    //         return this.auditService.feasibilityReport(this.dialogData.auditId);
-    //       else
-    //         return this.auditService.cehReport(this.dialogData.auditId);
-    //     })
-    //   ).subscribe((response: Blob) => {
-    //     if (!this.isBlob(response)) {
-    //       this.auditService.isCompleted = true;
-    //       return;
-    //     }
-    //     let blob : any = null;
-    //     if (type != "ceh")
-    //       blob = new Blob([response], { type: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' });
-    //     else
-    //       blob = new Blob([response], { type: 'text/csv' });
-
-    //     const url = window.URL.createObjectURL(blob);
-    //     const a = document.createElement('a');
-    //     a.href = url;
-    //     a.download = type == "auditEnergy" ? "Energy_Audit_Report.docx" : type == "feasibility" ? "feasibility_report.docx" : "CEH_Microgrid.csv";
-    //     document.body.appendChild(a);
-    //     a.click();
-    //     this.auditService.isCompleted = true;
-    //     document.body.removeChild(a);
-    //     window.URL.revokeObjectURL(url);
-    // });
   }
-  // isBlob(variable: any): variable is Blob {
-  //   return variable instanceof Blob;
-  // }
 }
