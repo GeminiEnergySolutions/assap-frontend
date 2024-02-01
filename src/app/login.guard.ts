4512ddfceb0a2d490dc7a16e998390a5b067a26b
Adrian Kunz <a.kunz@uni-kassel.de>
2024-02-01 22:00:02 +0100

    refactor: Make LoginGuard a CanActivateFn

--- src/app/login.guard.ts
+++ src/app/login.guard.ts
@@ -1,19 +1,10 @@
-import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
+import {inject} from '@angular/core';
+import {CanActivateFn, Router} from '@angular/router';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class LoginGuard  {
-  constructor(private router: Router) {}
-
-  canActivate(): boolean {
-    const token = localStorage.getItem('accessToken');
-    if (token) {
-      this.router.navigate(['/audits']);
-      return true;
-    } else {
-      return true;
-    }
+export const LoginGuard: CanActivateFn = async () => {
+  const token = localStorage.getItem('accessToken');
+  if (token) {
+    await inject(Router).navigate(['/audits']);
   }
-}
+  return true;
+};
