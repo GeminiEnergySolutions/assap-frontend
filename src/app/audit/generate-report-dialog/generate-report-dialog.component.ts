407cadb93e67db4473dd7ff8a82f389958b5c9a7
Adrian Kunz <a.kunz@uni-kassel.de>
2024-02-01 20:11:24 +0100

    refactor: Ignore isCompleted in generate report

--- src/app/audit/generate-report-dialog/generate-report-dialog.component.ts
+++ src/app/audit/generate-report-dialog/generate-report-dialog.component.ts
@@ -1,6 +1,5 @@
 import {Component} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
-import {AuditService} from 'src/app/shared/services/audit.service';
 import {AuthService} from 'src/app/shared/services/auth.service';
 import {environment} from 'src/environments/environment.prod';
 
@@ -10,26 +9,14 @@ import {environment} from 'src/environments/environment.prod';
   styleUrls: ['./generate-report-dialog.component.scss'],
 })
 export class GenerateReportDialogComponent {
-  formData: any = { type: 'energyAudit' };
-  tabType: string = 'energyAudit';
   serverUrl?: string = environment.url + "api/";
 
   constructor(
-    public auditService: AuditService,
     private authService: AuthService,
     private route: ActivatedRoute
   ) {
-    this.auditService.isCompleted = true;
   }
 
-  changeNav(type: any) {
-    if (this.tabType !== type) {
-      this.formData = { type };
-      this.tabType = type;
-    }
-  }
-
-  reportType = "auditEnergy";
   generateReport(type: string) {
     const {auditId} = this.route.snapshot.params;
     window.location.href = this.serverUrl + type + '?auditId=' + auditId + '&auth_token=' + this.authService.getAuthToken();
