95f81435a4b9060893f69cdc7684862282637a9f
Adrian Kunz <a.kunz@uni-kassel.de>
2024-02-03 15:33:06 +0100

    refactor: Type-specific form element things

--- src/app/shared/form-element/form-element.component.ts
+++ src/app/shared/form-element/form-element.component.ts
@@ -16,39 +16,28 @@ export class FormElementComponent {
     this.dirty.emit();
   }
 
-  formatDate(element: any) {
-    this.formData.data[element.key] = !this.formData.data[element.key] && element.isDateNow ? new Date().toISOString() : this.formData.data[element.key];
-    return '';
-  }
+  // TODO Autofill Dates:
+  //   this.formData.data[element.key] = !this.formData.data[element.key] && element.isDateNow ? new Date().toISOString() : this.formData.data[element.key];
 
-  changeCheckBox(event: any, key: string) {
-    this.formData.data[key] = event.target.checked;
+  changeDropDown(element: any) {
     this.setDirty();
-  }
-
-  changeDropDown(inputList: any[], key: string) {
-    if (inputList && inputList.length) {
-
-      const keyValue = this.formData.data[key]
-      let dependentInputList = inputList.filter((a: any) => a.dependentKeyValue != keyValue)
-
-      dependentInputList.forEach((element: any) => {
-
-        if (this.formData.data[element.key]) {
-          this.formData.data[element.key] = "";
-
-          if (element.inputList && element.inputList.length) {
-            dependentInputList.forEach((element1: any) => {
-
-              if (this.formData.data[element1.key])
-                this.formData.data[element1.key] = "";
-
-            });
+    if (!element.inputList?.length) {
+      return;
+    }
+    const keyValue = this.formData.data[element.key];
+    let dependentInputList = element.inputList.filter((a: any) => a.dependentKeyValue != keyValue);
+    for (const element1 of dependentInputList) {
+      if (!this.formData.data[element1.key]) {
+        continue;
+      }
+      this.formData.data[element1.key] = '';
+      if (element1.inputList && element1.inputList.length) {
+        for (const element2 of dependentInputList) { // FIXME this is probably wrong. Why would we loop over the same list again?
+          if (this.formData.data[element2.key]) {
+            this.formData.data[element2.key] = '';
           }
         }
-      });
+      }
     }
-
-    this.setDirty();
   }
 }
