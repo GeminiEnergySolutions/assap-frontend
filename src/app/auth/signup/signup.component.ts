fa2f70060b7c7da2355263f00f8868ff1066617b
Adrian Kunz <clashsoft@hotmail.com>
2024-02-04 20:56:16 +0100

    refactor: Simplify Signup component

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,30 +1,24 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { ToastService } from 'ng-bootstrap-ext';
-import { AuditService } from 'src/app/shared/services/audit.service';
-import { AuthService } from 'src/app/shared/services/auth.service';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {ToastService} from 'ng-bootstrap-ext';
+import {AuthService} from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
-
-  public signupForm!: FormGroup;
-  public selectedUserType: string = 'sandbox';
+  signupForm!: FormGroup;
+  selectedUserType = 'sandbox';
   submitting = false;
 
   constructor(
     private formBuilder: FormBuilder,
-    public auditService: AuditService,
-    public authService: AuthService,
+    private authService: AuthService,
     private router: Router,
     private toastService: ToastService,
-    private http: HttpClient // Inject the HttpClient
-
   ) {
   }
 
@@ -34,15 +28,9 @@ export class SignupComponent implements OnInit {
       Email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
       ConfirmPassword: ['', Validators.required],
-      state: ['']
+      state: [''],
     });
-
-    // Fetch and populate the states when the component initializes
-    // this.authService.allstates().subscribe((res: any) => {
-    //   this.states = res;//.map((stateObj: any) => stateObj.state);
-    // });
   }
-
 
   signUp() {
     let userData: any = {
@@ -52,20 +40,18 @@ export class SignupComponent implements OnInit {
       password_confirm: this.signupForm.get('ConfirmPassword')?.value,
     };
 
-
     if (this.selectedUserType === 'sandbox') {
       userData.role = 'guest';
     } else if (this.selectedUserType === 'dataCollector') {
       userData.role = 'dataCollector';
-      // userData.state = this.signupForm.get('state')?.value;
     }
 
     this.submitting = true;
     this.authService.signUp(userData).subscribe((res) => {
-        this.submitting = false;
-        this.toastService.success('Success', 'Registration successful!');
-        this.signupForm.reset();
-        this.router.navigate(['auth/login']);
+      this.submitting = false;
+      this.toastService.success('Success', 'Registration successful!');
+      this.signupForm.reset();
+      this.router.navigate(['auth/login']);
     }, error => {
       this.submitting = false;
       this.toastService.error('Error', 'Registration failed', error);
