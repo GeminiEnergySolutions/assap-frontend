8ae579aa6a75a4fa2eadc683d083b27d04efac2f
Adrian Kunz <clashsoft@hotmail.com>
2024-02-10 16:10:17 +0100

    refactor: Remove offline code from pre-audit

--- src/app/audit/pre-audit/pre-audit.component.ts
+++ src/app/audit/pre-audit/pre-audit.component.ts
@@ -1,6 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {ToastService} from 'ng-bootstrap-ext';
-import {forkJoin} from 'rxjs';
 import {AuditService} from 'src/app/shared/services/audit.service';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {AuthService} from 'src/app/shared/services/auth.service';
@@ -9,14 +7,14 @@ import {AddDataCollectorModalComponent} from '../add-data-collector-modal/add-da
 @Component({
   selector: 'app-pre-audit',
   templateUrl: './pre-audit.component.html',
-  styleUrls: ['./pre-audit.component.scss']
+  styleUrls: ['./pre-audit.component.scss'],
 })
 export class PreAuditComponent implements OnInit {
   audits: any = [];
 
-  constructor(private auditService: AuditService,
+  constructor(
+    private auditService: AuditService,
     public authService: AuthService,
-    private toastService: ToastService,
     private modalService: NgbModal,
   ) {
   }
@@ -40,18 +38,17 @@ export class PreAuditComponent implements OnInit {
       return;
     }
 
-    this.auditService.createAudit({ auditName: name }).subscribe((res: any) => {
+    this.auditService.createAudit({auditName: name}).subscribe((res: any) => {
       this.audits.push(res.data);
     });
   }
 
-  // rename(audit: Audit) {
   rename(audit: any) {
     const name = prompt('Rename Audit', audit.auditName);
     if (!name) {
       return;
     }
-    let auditData = { ...audit, auditName: name };
+    let auditData = {...audit, auditName: name};
 
     this.auditService.updateAudit(auditData).subscribe((res: any) => {
       let index = this.audits.indexOf(audit);
@@ -59,7 +56,6 @@ export class PreAuditComponent implements OnInit {
     });
   }
 
-  // delete(audit: Audit) {
   delete(audit: any) {
     if (!confirm(`Are you sure you want to delete '${audit.auditName}'?`)) {
       return;
@@ -67,44 +63,12 @@ export class PreAuditComponent implements OnInit {
     this.auditService.deleteAudit(audit.auditId).subscribe((res: any) => {
       let index = this.audits.findIndex((a: any) => a.auditId === audit.auditId);
       this.audits.splice(index, 1);
-    })
-  }
-
-  // download(audit: Audit) {
-  download(audit: any) {
-    if (audit.pendingChanges && !confirm(`Are you sure you want to discard ${audit.pendingChanges} pending changes? This cannot be undone.`)) {
-      return;
-    }
-    // this.schemaService.loadSchemas().subscribe();
-    // this.offlineAuditService.save(audit);
-    // this.featureService.saveAll({auditId: audit.auditId});
-  }
-
-  // upload(audit: Audit) {
-  upload(audit: any) {
-    forkJoin([
-      // this.auditService.upload(audit),
-      // this.featureService.upload({auditId: audit.auditId}),
-    ]).subscribe(undefined, error => {
-      this.toastService.error('Audit', 'Failed to upload audit', error);
     });
   }
-  openAddDataCollectorModal(audit: any) {
-    const modalRef = this.modalService.open(AddDataCollectorModalComponent, { size: 'lg' });
-    modalRef.componentInstance.audit = audit;
-  }
 
-  // deleteOffline(audit: Audit) {
-  deleteOffline(audit: any) {
-    const message = 'Are you sure you want to discard the offline copy' + (
-      audit.pendingChanges
-        ? ', including ' + audit.pendingChanges + ' pending changes? This cannot be undone.'
-        : '? This can be undone once an internet connection is available.'
-    );
-    if (!confirm(message)) {
-      return;
-    }
-    // this.offlineAuditService.delete(audit);
+  openAddDataCollectorModal(audit: any) {
+    const modalRef = this.modalService.open(AddDataCollectorModalComponent, {size: 'lg'});
+    modalRef.componentInstance.audit = audit;
   }
 
 }
