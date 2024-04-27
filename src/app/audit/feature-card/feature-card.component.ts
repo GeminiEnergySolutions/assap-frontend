8726740a0b81e29fff58fad492eb80185d7b4c2f
Adrian Kunz <a.kunz@uni-kassel.de>
2024-04-27 20:43:42 +0200

    feat: Implement progress view in feature-cards

--- src/app/audit/feature-card/feature-card.component.ts
+++ src/app/audit/feature-card/feature-card.component.ts
@@ -1,11 +1,28 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {AuditService, PercentageQuery} from '../../shared/services/audit.service';
+import {PercentageCompletion} from '../../shared/model/percentage-completion.interface';
 
 @Component({
   selector: 'app-feature-card',
   templateUrl: './feature-card.component.html',
   styleUrl: './feature-card.component.scss',
 })
-export class FeatureCardComponent {
+export class FeatureCardComponent implements OnInit {
   @Input({required: true}) title!: string;
   @Input({required: true}) routerLink!: any[] | string;
+
+  @Input() percentageQuery?: PercentageQuery;
+
+  progress?: PercentageCompletion;
+
+  constructor(
+    private readonly auditService: AuditService,
+  ) {
+  }
+
+  ngOnInit() {
+    this.percentageQuery && this.auditService.getPercentage(this.percentageQuery).subscribe(res => {
+      this.progress = res;
+    });
+  }
 }
