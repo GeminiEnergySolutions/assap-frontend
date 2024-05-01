de6e9adfc01ee738cd950603723e8edc406ea248
nmalikgeminie <nmalik@geminiesolutions.com>
2024-05-01 12:25:42 -0400

    update env prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   url: 'http://localhost:8000/'
-  // url: 'https://conserve.geminiesolutions.com/'
+  // url: 'https://api.geminiesolutions.com/'
 };
