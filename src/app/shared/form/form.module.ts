c3df22034df70d237596c594e9105d29475eef11
Adrian Kunz <clashsoft@hotmail.com>
2024-06-29 21:25:44 +0200

    feat: Change save button color when pending changes

--- src/app/shared/form/form.module.ts
+++ src/app/shared/form/form.module.ts
@@ -1,7 +1,7 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {FormsModule as NgFormsModule} from '@angular/forms';
-import {NgbAccordionModule} from '@ng-bootstrap/ng-bootstrap';
+import {NgbAccordionModule, NgbTooltip} from '@ng-bootstrap/ng-bootstrap';
 import {FormInputTypePipe} from './form-input-type.pipe';
 import {FormComponent} from './form/form.component';
 import {FormElementComponent} from './form-element/form-element.component';
@@ -21,6 +21,7 @@ import {SharedModule} from '../shared.module';
         NgFormsModule,
         NgbAccordionModule,
         SharedModule,
+        NgbTooltip,
     ],
 })
 export class FormsModule {
