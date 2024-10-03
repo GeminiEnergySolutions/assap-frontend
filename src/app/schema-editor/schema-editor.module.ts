2ce419bcc75b6c376a911b1b3600d65350f6334b
Adrian Kunz <clashsoft@hotmail.com>
2024-10-03 15:16:32 +0200

    feat: Add schema editor module

---
+++ src/app/schema-editor/schema-editor.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+import {SchemaEditorRoutingModule} from './schema-editor-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SchemaEditorRoutingModule,
+  ],
+})
+export class SchemaEditorModule {
+}
