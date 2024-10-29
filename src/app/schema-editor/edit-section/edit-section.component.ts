c710fdd8c3fcb63ccd9932c2a9b425a7a0c6f8c5
Adrian Kunz <clashsoft@hotmail.com>
2024-10-29 22:44:05 +0100

    feat(schema-editor): Save sections

--- src/app/schema-editor/edit-section/edit-section.component.ts
+++ src/app/schema-editor/edit-section/edit-section.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {SchemaSection} from '../../shared/model/schema.interface';
 import {SchemaContextService} from '../schema-context.service';
+import {SchemaService} from '../../shared/services/schema.service';
 
 @Component({
   selector: 'app-edit-section',
@@ -10,16 +11,24 @@ import {SchemaContextService} from '../schema-context.service';
 })
 export class EditSectionComponent implements OnInit {
   section: SchemaSection = {id: 0, name: '', schema: []};
+  dirty = false;
 
   constructor(
     private route: ActivatedRoute,
     private schemaContext: SchemaContextService,
+    private schemaService: SchemaService,
   ) {
   }
 
   ngOnInit() {
     this.route.params.subscribe(({section}) => {
       this.section = this.schemaContext.schema.find(s => s.id == section) ?? this.section;
+    });
+  }
+
+  save() {
+    this.schemaService.updateSchemaSection(this.schemaContext.kind, this.section.id, this.section).subscribe(() => {
+      this.dirty = false;
     });
   }
 }
