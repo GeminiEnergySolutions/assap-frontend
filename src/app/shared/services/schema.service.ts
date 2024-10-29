e29de8d4005db7bc1f21c1209e6611e0c0cbdb34
Adrian Kunz <clashsoft@hotmail.com>
2024-10-29 22:01:12 +0100

    feat: Add schema edit endpoints

--- src/app/shared/services/schema.service.ts
+++ src/app/shared/services/schema.service.ts
@@ -1,10 +1,10 @@
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
-import {SchemaResponse} from '../model/schema.interface';
+import {SchemaResponse, SchemaSection} from '../model/schema.interface';
 import {environment} from '../../../environments/environment.prod';
 import {HttpClient} from '@angular/common/http';
 
-type SchemaKind = 'preAudit' | 'ceh' | 'grants' | 'zone' | `equipment/${number}`;
+export type SchemaKind = 'preAudit' | 'ceh' | 'grants' | 'zone' | `equipment/${number}`;
 
 @Injectable({providedIn: 'root'})
 export class SchemaService {
@@ -15,5 +15,18 @@ export class SchemaService {
 
   getSchema(kind: SchemaKind): Observable<SchemaResponse> {
     return this.http.get<SchemaResponse>(`${environment.url}api/schemas/${kind}`);
+  }
+
+  createSchemaSection(kind: SchemaKind, data: SchemaSection): Observable<{ data: SchemaSection }> {
+    return this.http.post<{ data: SchemaSection }>(`${environment.url}api/schemas/${kind}/`, data);
+  }
+  getSchemaSection(kind: SchemaKind, section: number): Observable<{ data: SchemaSection }> {
+    return this.http.get<{ data: SchemaSection }>(`${environment.url}api/schemas/${kind}/${section}/`);
+  }
+  updateSchemaSection(kind: SchemaKind, section: number, data: SchemaSection): Observable<{}> {
+    return this.http.put<{}>(`${environment.url}api/schemas/${kind}/${section}/`, data);
+  }
+  deleteSchemaSection(kind: SchemaKind, section: number): Observable<{}> {
+    return this.http.delete<{}>(`${environment.url}api/schemas/${kind}/${section}/`);
   }
 }
