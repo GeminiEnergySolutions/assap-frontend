2d55dd7a0df3d5739dc97795bc090a604811ed9c
Adrian Kunz <clashsoft@hotmail.com>
2024-12-22 18:27:33 +0100

    refactor: Remove photos pipe

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,7 +4,6 @@ import {RouterModule} from '@angular/router';
 import {NgbDropdownModule} from '@ng-bootstrap/ng-bootstrap';
 import {MasterDetailComponent} from './components/master-detail/master-detail.component';
 import {OptionDropdownComponent} from './components/option-dropdown/option-dropdown.component';
-import {PhotosPipe} from './pipe/photos.pipe';
 import {SafePipe} from './pipe/safe.pipe';
 import {FeatureCardComponent} from './components/feature-card/feature-card.component';
 import {ProgressBarComponent} from './components/progress-bar/progress-bar.component';
@@ -20,7 +19,6 @@ import {ExpressionErrorPipe} from './pipe/expression-error.pipe';
     FeatureCardComponent,
     ProgressBarComponent,
     PhotoCaptureComponent,
-    PhotosPipe,
     SafePipe,
     EvalPipe,
     ExpressionErrorPipe,
@@ -36,7 +34,6 @@ import {ExpressionErrorPipe} from './pipe/expression-error.pipe';
     FeatureCardComponent,
     ProgressBarComponent,
     PhotoCaptureComponent,
-    PhotosPipe,
     SafePipe,
     EvalPipe,
     ExpressionErrorPipe,
