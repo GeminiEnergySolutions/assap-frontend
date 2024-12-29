3c2da46e1169007551577d3df71e57facb14de6a
Adrian Kunz <a.kunz@uni-kassel.de>
2024-12-29 19:30:42 +0100

    feat: Audit detail breadcrumb

--- src/app/audit/audit-detail/audit-detail.component.ts
+++ src/app/audit/audit-detail/audit-detail.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {switchMap} from 'rxjs';
 import {AuditService} from '../../shared/services/audit.service';
 import {Audit} from '../../shared/model/audit.interface';
+import {Breadcrumb, BreadcrumbService} from '../../shared/services/breadcrumb.service';
 
 @Component({
   selector: 'app-audit-detail',
@@ -10,20 +11,25 @@ import {Audit} from '../../shared/model/audit.interface';
   styleUrls: ['./audit-detail.component.scss'],
   standalone: false,
 })
-export class AuditDetailComponent implements OnInit {
+export class AuditDetailComponent implements OnInit, OnDestroy {
   audit?: Audit;
 
   constructor(
     public auditService: AuditService,
+    private breadcrumbService: BreadcrumbService,
     public route: ActivatedRoute,
   ) {
   }
 
   ngOnInit(): void {
+    const breadcrumb: Breadcrumb = {label: '<Audit>', routerLink: '.', relativeTo: this.route};
+    this.breadcrumbService.pushBreadcrumb(breadcrumb);
+
     this.route.params.pipe(
       switchMap(({aid}) => this.auditService.getSingleAudit(aid)),
     ).subscribe(({data}) => {
       this.audit = data;
+      breadcrumb.label = data.auditName;
     });
 
     this.route.params.pipe(
@@ -32,5 +38,9 @@ export class AuditDetailComponent implements OnInit {
         auditId: aid,
       })),
     ).subscribe(res => this.auditService.currentProgress = res);
+  }
+
+  ngOnDestroy() {
+    this.breadcrumbService.popBreadcrumb();
   }
 }
