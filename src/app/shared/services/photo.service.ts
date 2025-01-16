416a1c8166a81667065b66e9803404efdb0408c7
Adrian Kunz <clashsoft@hotmail.com>
2025-01-16 23:26:33 +0100

    feat: New photo upload

--- src/app/shared/services/photo.service.ts
+++ src/app/shared/services/photo.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
+import {Observable, switchMap} from 'rxjs';
 import {Response} from '../model/response.interface';
 import {Photo, PhotoInfo, PhotoQuery} from '../model/photo.interface';
 import {HttpClient} from '@angular/common/http';
@@ -24,12 +24,18 @@ export class PhotoService {
     return this.http.delete<Response>(`${environment.url}api/audit/${auditId}/photos`, {params: {id}});
   }
 
-  uploadPhoto(info: PhotoInfo, file: File): Observable<Response> {
+  uploadPhoto(info: PhotoInfo, file: File): Observable<object> {
     const formData = new FormData();
     for (const [key, value] of Object.entries(info)) {
       formData.append(key, String(value));
     }
-    formData.append('photo', file, file.name);
-    return this.http.post<Response>(`${environment.url}api/audit/${info.auditId}/photos`, formData);
+    // Construct filename from current date and file extension (ignore filename, must be less than 50 characters)
+    formData.append('photo', Date.now() + file.name.substring(file.name.lastIndexOf('.')));
+    formData.append('type', file.type);
+    return this.http.post<{ upload_url: string }>(`${environment.url}api/audit/${info.auditId}/photos`, formData).pipe(
+      switchMap(({upload_url}) => this.http.put(upload_url, file, {
+        responseType: 'blob',
+      })),
+    );
   }
 }
