41a320c1b949d87675a3944cfb05f61f038e9501
Adrian Kunz <clashsoft@hotmail.com>
2025-02-28 19:43:49 +0100

    feat(schema-editor): Duplicate subtype and schema

--- src/app/schema-editor/select-schema/select-schema.component.ts
+++ src/app/schema-editor/select-schema/select-schema.component.ts
@@ -1,15 +1,19 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, RouterLink} from '@angular/router';
+import {ToastService} from '@mean-stream/ngbx';
+import {NgbTooltip} from '@ng-bootstrap/ng-bootstrap';
+import {concatMap, mergeMap, switchMap, tap, toArray} from 'rxjs';
 
 import {EquipmentCategory, EquipmentType} from '../../shared/model/equipment.interface';
 import {BreadcrumbService} from '../../shared/services/breadcrumb.service';
 import {EquipmentService} from '../../shared/services/equipment.service';
+import {SchemaService} from '../../shared/services/schema.service';
 
 @Component({
   selector: 'app-select-schema',
   templateUrl: './select-schema.component.html',
   styleUrl: './select-schema.component.scss',
-  imports: [RouterLink],
+  imports: [RouterLink, NgbTooltip],
 })
 export class SelectSchemaComponent implements OnInit {
   categories: EquipmentCategory[] = [];
@@ -19,6 +23,8 @@ export class SelectSchemaComponent implements OnInit {
     private equipmentService: EquipmentService,
     private breadcrumbService: BreadcrumbService,
     private route: ActivatedRoute,
+    private schemaService: SchemaService,
+    private toastService: ToastService,
   ) {
   }
 
@@ -48,6 +54,34 @@ export class SelectSchemaComponent implements OnInit {
       name,
     }).subscribe(({data}) => {
       this.equipmentTypes[category.id]?.push(data);
+    });
+  }
+
+  duplicateSubType(subType: EquipmentType) {
+    const name = prompt('Enter the name of the new type', subType.name);
+    if (!name) {
+      return;
+    }
+
+    this.equipmentService.createEquipmentType(subType.equipmentId, {
+      equipmentId: subType.equipmentId,
+      name,
+    }).pipe(
+      tap(({data}) => this.equipmentTypes[subType.equipmentId]?.push(data)),
+      switchMap(newSubType => this.schemaService.getSchema(`equipment/${subType.id}`).pipe(
+        mergeMap(({data}) => data),
+        concatMap(schema => this.schemaService.createSchemaSection(`equipment/${subType.equipmentId}`, {
+          ...schema,
+          id: 0,
+          typeId: newSubType.data.id,
+        })),
+        toArray(),
+      )),
+    ).subscribe(result => {
+      this.toastService.success(
+        'Duplicated Subtype',
+        `Successfully duplicated subtype ${subType.name} and ${result.length} schema sections`,
+      );
     });
   }
 }
