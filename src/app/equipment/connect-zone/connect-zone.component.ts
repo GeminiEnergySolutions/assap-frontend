dad84d8e7afcfc23d1f96eaa87e6d6cc6522aedd
Adrian Kunz <a.kunz@uni-kassel.de>
2025-05-29 20:14:54 +0200

    refactor: Use set for connected zones

--- src/app/equipment/connect-zone/connect-zone.component.ts
+++ src/app/equipment/connect-zone/connect-zone.component.ts
@@ -1,22 +1,21 @@
 import {Component, OnInit} from '@angular/core';
-import {NgbTooltip} from '@ng-bootstrap/ng-bootstrap';
-import {SearchPipe} from '../../shared/pipe/search.pipe';
-import {EquipmentService} from '../../shared/services/equipment.service';
-import {ModalModule, ToastService} from '@mean-stream/ngbx';
+import {FormsModule} from '@angular/forms';
 import {ActivatedRoute} from '@angular/router';
+import {ModalModule, ToastService} from '@mean-stream/ngbx';
 import {switchMap} from 'rxjs';
 import {ZoneWithHvacConnected} from '../../shared/model/zone.interface';
-import {FormsModule} from '@angular/forms';
+import {SearchPipe} from '../../shared/pipe/search.pipe';
+import {EquipmentService} from '../../shared/services/equipment.service';
 
 @Component({
   selector: 'app-connect-zone',
   templateUrl: './connect-zone.component.html',
   styleUrl: './connect-zone.component.scss',
-  imports: [ModalModule, FormsModule, NgbTooltip, SearchPipe],
+  imports: [ModalModule, FormsModule, SearchPipe],
 })
 export class ConnectZoneComponent implements OnInit {
   connectedZones: ZoneWithHvacConnected[] = [];
-  selection: Record<number, boolean> = {};
+  selection = new Set<number>;
   search = '';
 
   constructor(
@@ -31,11 +30,16 @@ export class ConnectZoneComponent implements OnInit {
       switchMap(({aid, zid}) => this.equipmentService.getConnectedZones(aid, zid)),
     ).subscribe(result => {
       this.connectedZones = result.data;
+      for (const zone of result.data) {
+        if (zone.isConnected) {
+          this.selection.add(zone.zoneId);
+        }
+      }
     });
   }
 
   save() {
-    const zoneIds = Object.entries(this.selection).filter(([, value]) => value).map(([key]) => +key);
+    const zoneIds = [...this.selection];
     const {aid, zid, tid} = this.route.snapshot.params;
     this.equipmentService.setConnectedZones(+aid, +zid, +tid, zoneIds).subscribe(() => {
       this.toastService.success('Connect Zones', `Successfully connected ${zoneIds.length} zones to HVAC unit.`);
