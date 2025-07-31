1577d6ddb8b0d5220af024e6e75457a88a7ac49c
Adrian Kunz <a.kunz@uni-kassel.de>
2025-07-31 23:18:31 +0200

    feat: Select element modal

--- src/app/tutorial/select-element/select-element.component.ts
+++ src/app/tutorial/select-element/select-element.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, inject, OnDestroy, TemplateRef, ViewChild} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {ModalModule} from '@mean-stream/ngbx';
+import {NgbModal, NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-select-element',
-  imports: [],
+  imports: [
+    ModalModule,
+  ],
   templateUrl: './select-element.component.html',
   styleUrl: './select-element.component.scss'
 })
-export class SelectElementComponent {
+export class SelectElementComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('content', {static: true}) content!: TemplateRef<unknown>;
 
+  readonly ngbModal = inject(NgbModal);
+  readonly route = inject(ActivatedRoute);
+  readonly router = inject(Router);
+
+  modalRef?: NgbModalRef;
+
+  ngAfterViewInit() {
+    this.modalRef = this.ngbModal.open(this.content, {
+      beforeDismiss: () => this.router.navigate(['..'], {relativeTo: this.route}).then(() => true),
+      size: 'sm',
+      backdrop: false,
+      windowClass: 'pointer-events-none',
+      modalDialogClass: 'me-3',
+      fullscreen: false,
+    });
+  }
+
+  ngOnDestroy() {
+    this.modalRef?.dismiss();
+  }
 }
