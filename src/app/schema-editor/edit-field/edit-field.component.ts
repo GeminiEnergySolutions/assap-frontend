33c54b497b44960e5e52cff2f6fdb718e42d349a
Adrian Kunz <a.kunz@uni-kassel.de>
2025-10-22 09:11:14 +0200

    feat: Add time data type and remove special date input type

--- src/app/schema-editor/edit-field/edit-field.component.ts
+++ src/app/schema-editor/edit-field/edit-field.component.ts
@@ -87,7 +87,7 @@ export class EditFieldComponent implements OnInit, OnDestroy {
           type: 'select',
           hint: 'The type of data this field represents',
           title: 'Data Type',
-          values: ['text', 'number', 'integer', 'date', 'bool'],
+          values: ['text', 'number', 'integer', 'date', 'time', 'bool'],
           required: true,
           inputList: [
             // --------------- Text ---------------
@@ -167,7 +167,7 @@ export class EditFieldComponent implements OnInit, OnDestroy {
               hint: 'The physical unit or currency of the input field',
               required: false,
             },
-            // --------------- Date ---------------
+            // --------------- Date and Time ---------------
             {
               dependentKeyValue: 'date',
               key: 'isDateNow',
@@ -185,15 +185,23 @@ export class EditFieldComponent implements OnInit, OnDestroy {
               hint: 'The default date value for this field',
             },
             {
-              dependentKeyValue: 'date',
+              dependentKeyValue: 'time',
+              key: 'defaultValue',
+              dataType: 'time',
+              type: 'textBox',
+              title: 'Default Time Value',
+              hint: 'The default time value for this field',
+            },
+            {
+              dependentKeyValue: ['date', 'time'],
               key: 'type',
               dataType: 'text',
               type: 'select',
               title: 'Input Type',
               hint: 'The type of input field',
-              values: ['date'],
+              values: ['textBox'],
               disabled: true,
-              defaultValue: 'date',
+              defaultValue: 'textBox',
             },
             // --------------- Boolean ---------------
             {
