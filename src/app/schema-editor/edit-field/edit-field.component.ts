6019c14583ea0c64e62ff6d4d8e68d35208a3778
Adrian Kunz <a.kunz@uni-kassel.de>
2025-10-22 08:58:22 +0200

    feat(schema-editor): Copy and paste validation

--- src/app/schema-editor/edit-field/edit-field.component.ts
+++ src/app/schema-editor/edit-field/edit-field.component.ts
@@ -9,7 +9,7 @@ import {EditorComponent} from '../../shared/components/editor/editor.component';
 
 import {FormComponent} from '../../shared/form/form/form.component';
 import {icons} from '../../shared/icons';
-import {SchemaElement, SchemaSection, SchemaSubElement} from '../../shared/model/schema.interface';
+import {SchemaElement, SchemaRequirement, SchemaSection, SchemaSubElement} from '../../shared/model/schema.interface';
 import {Breadcrumb, BreadcrumbService} from '../../shared/services/breadcrumb.service';
 import {CopyPasteService} from '../../shared/services/copy-paste.service';
 import {SchemaContextService} from '../schema-context.service';
@@ -342,6 +342,27 @@ export class EditFieldComponent implements OnInit, OnDestroy {
   removeValidation(index: number) {
     this.field.validations?.splice(index, 1);
     this.setDirty();
+  }
+
+  copyValidation(validation: SchemaRequirement) {
+    this.copyPasteService.copy(validation).subscribe(() => {
+      this.toastService.success('Copied to Clipboard', 'Validation copied to clipboard');
+    });
+  }
+
+  pasteValidation() {
+    this.copyPasteService.paste<SchemaRequirement>([
+      'level',
+      'if',
+      'message',
+    ]).subscribe({
+      next: validation => {
+        (this.field.validations ??= []).push(validation);
+        this.setDirty();
+        this.toastService.success('Pasted', `Successfully pasted validation`);
+      },
+      error: error => this.toastService.error('Failed to Paste', 'Clipboard is empty or does not contain a valid validation', error),
+    });
   }
 
   addSubfield() {
