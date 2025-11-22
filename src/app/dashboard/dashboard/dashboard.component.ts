a9bbe81369f13f803ed0cd1c78d14f60310d2bea
Adrian Kunz <a.kunz@uni-kassel.de>
2025-11-22 15:03:50 +0100

    feat: Show GHG savings on a map

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,7 +1,18 @@
 import {DecimalPipe} from '@angular/common';
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, inject, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {registerables} from 'chart.js';
+import {
+  ChoroplethChart,
+  ChoroplethController,
+  ColorScale,
+  GeoFeature,
+  ProjectionScale,
+  topojson,
+} from 'chartjs-chart-geo';
+import {BaseChartDirective, provideCharts, withDefaultRegisterables} from 'ng2-charts';
 import {switchMap} from 'rxjs';
+import us from 'us-atlas/states-10m.json';
 import {DashboardService, SummaryResult} from '../dashboard.service';
 import equivalents from './equivalents.json';
 
@@ -9,11 +20,17 @@ import equivalents from './equivalents.json';
   selector: 'app-dashboard',
   imports: [
     DecimalPipe,
+    BaseChartDirective,
+  ],
+  providers: [
+    provideCharts(withDefaultRegisterables(ChoroplethController, GeoFeature, ProjectionScale, ColorScale, ...registerables)),
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent implements OnInit {
+  @ViewChild(BaseChartDirective) coffeeChart?: BaseChartDirective;
+
   private readonly route = inject(ActivatedRoute);
   private readonly dashboardService = inject(DashboardService);
 
@@ -26,7 +43,14 @@ export class DashboardComponent implements OnInit {
   energyEquivalent = this.random(equivalents.energy);
   costsEquivalent = this.random(equivalents.costs);
 
+  mapData?: ChoroplethChart['data'];
+
   ngOnInit() {
+    // @ts-expect-error taken from the example, can't bother with the types
+    const nation = topojson.feature(us, us.objects.nation).features[0];
+    // @ts-expect-error see above
+    const states = topojson.feature(us, us.objects.states).features;
+
     this.route.queryParams.pipe(
       switchMap(({state}) => this.dashboardService.getDataSummary(+state)),
     ).subscribe(({data}) => {
@@ -35,6 +59,21 @@ export class DashboardComponent implements OnInit {
       this.totalGHGSavings = data.reduce((a, c) => a + c.GHG_emissions_savings, 0);
       this.totalEnergySavings = data.reduce((a, c) => a + c.kBTU_per_year_savings, 0);
       this.totalCostSavings = data.reduce((a, c) => a + c.cost_per_year_savings, 0);
+
+      this.mapData = {
+        labels: states.map((d: any) => d.properties.name),
+        datasets: [{
+          label: 'States',
+          outline: nation,
+          data: states.map((d: any) => {
+            const stateName: string = d.properties.name;
+            return ({
+              feature: d,
+              value: data.find(state => state.state_name === stateName)?.GHG_emissions_savings ?? 0,
+            });
+          }),
+        }],
+      };
     });
   }
 
