4cea6214b7f8b2b9b0f80c3db8f178c53b36bffa
nmalikgeminie <117112074+nmalikgeminie@users.noreply.github.com>
2025-12-08 19:41:00 +0500

    fix: update upload photo response and payload (#73)
    * update upload photo response and payload
    
    * Update src/app/shared/services/photo.service.ts
    
    Co-authored-by: Adrian Kunz <clashsoft@hotmail.com>
    
    * Update src/app/shared/services/photo.service.ts
    
    Co-authored-by: Adrian Kunz <clashsoft@hotmail.com>
    
    ---------
    
    Co-authored-by: Adrian Kunz <clashsoft@hotmail.com>

--- src/app/shared/services/photo.service.ts
+++ src/app/shared/services/photo.service.ts
@@ -30,12 +30,10 @@ export class PhotoService {
       formData.append(key, String(value));
     }
     // Construct filename from current date and file extension (ignore filename, must be less than 50 characters)
-    formData.append('photo', Date.now() + file.name.substring(file.name.lastIndexOf('.')));
+    formData.append('photo', file.name);
     formData.append('type', file.type);
-    return this.http.post<{ upload_url: string }>(`${environment.url}api/audit/${info.auditId}/photos`, formData).pipe(
-      switchMap(({upload_url}) => this.http.put(upload_url, file, {
-        responseType: 'blob',
-      })),
+    return this.http.post<Response<{upload_url: string;}>>(`${environment.url}api/audit/${info.auditId}/photos`, formData).pipe(
+      switchMap(({data}) => this.http.put(data.upload_url, file, {responseType: 'blob'}),
     );
   }
 }
