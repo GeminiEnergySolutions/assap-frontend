7d69344c427f26758f81311b2c785d9205abff84
nmalikgeminie <117112074+nmalikgeminie@users.noreply.github.com>
2025-12-08 09:30:27 -0500

    Update src/app/shared/services/photo.service.ts
    Co-authored-by: Adrian Kunz <clashsoft@hotmail.com>

--- src/app/shared/services/photo.service.ts
+++ src/app/shared/services/photo.service.ts
@@ -32,7 +32,7 @@ export class PhotoService {
     // Construct filename from current date and file extension (ignore filename, must be less than 50 characters)
     formData.append('photo', file.name);
     formData.append('type', file.type);
-    return this.http.post<Response<{data: {}; upload_url: string;}>>(`${environment.url}api/audit/${info.auditId}/photos`, formData).pipe(
+    return this.http.post<Response<{upload_url: string;}>>(`${environment.url}api/audit/${info.auditId}/photos`, formData).pipe(
       switchMap(({data}) => {
         return this.http.put(data.upload_url, file, {responseType: 'blob'})
       }),
